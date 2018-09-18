85663d42a05ffb12be276c83c3aae949c030b6da
Cheryl K <cherylkelson@icloud.com>
2018-09-18 16:22:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,9 +16,8 @@ end
 def find_the_cheese(maybe_cheese)
  types_of_cheese  = ["cheddar", "gouda", "camembert"]
   maybe_cheese.find do |cheese |
-   types_of_cheese.include? (cheese)
-    
-end 
+    types_of_cheese.include? (cheese)
+  end 
   
 end
 
